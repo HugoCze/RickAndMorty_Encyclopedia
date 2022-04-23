403047b47e2d70fe8f5d7d58afb1d59ecbe29442
Hugo <hugoczerniawski@gmail.com>
2022-04-23 11:27:35 +0200

    Initial Commit

---
+++ RickAndMortyApiVol2/RickAndMortyApiVol2App.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiVol2App.swift
+//  RickAndMortyApiVol2
+//
+//  Created by Hugo Czerniawski on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiVol2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
